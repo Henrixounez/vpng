c7ab7d358abac4c2fed704970b25a956613a6154
Ivan Vatlin <16368461+jeffangelion@users.noreply.github.com>
2021-05-14 14:11:23 +0300

    Remove redundant subarray() function
    + replace subarray() w/ array slices
    + parse.v:170 replaced w/ array slice too

--- utils.v
+++ utils.v
@@ -11,11 +11,3 @@ fn byte_to_int(bytes []byte) int {
 fn int_to_bytes(nb int) []byte {
 	return [byte((nb >> 24) & 0xFF), byte((nb >> 16) & 0xFF), byte((nb >> 8) & 0xFF), byte(nb & 0xFF)]
 }
-
-fn subarray(arr []byte, start int, end int) []byte {
-	mut res := []byte{len: end - start}
-	for i in 0 .. (end - start) {
-		res[i] = arr[start + i]
-	}
-	return res
-}
